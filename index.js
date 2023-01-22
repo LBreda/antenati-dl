64637e6a73ddac5994ba128f4c261dbf5ad08576
Lorenzo Breda <lorenzo@lbreda.com>
2023-01-22 19:12:47 +0100

    :sparkles: :arrow-up: The required origin URI changed, version upgrade

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ let limit = parseInt(process.argv[4]) || undefined
 const reqHeaders = {
     headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0',
-        'Origin': 'https://www.antenati.san.beniculturali.it',
-        'Referer': 'https://www.antenati.san.beniculturali.it/'
+        'Origin': 'https://antenati.cultura.gov.it',
+        'Referer': 'https://antenati.cultura.gov.it',
     }
 }
 
