45aca405b9e28fdeb0a48ece041649c18b6662b1
Lorenzo Breda <lorenzo@lbreda.dev>
2025-07-24 00:19:36 +0200

    :arrow-up: Added a trailing slash on the Referer header and upgraded deps

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ let limit = parseInt(process.argv[4]) || undefined
 
 const reqHeaders = {
     headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0) Gecko/20100101 Firefox/97.0',
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0',
         'Origin': 'https://antenati.cultura.gov.it',
-        'Referer': 'https://antenati.cultura.gov.it',
+        'Referer': 'https://antenati.cultura.gov.it/',
     }
 }
 
